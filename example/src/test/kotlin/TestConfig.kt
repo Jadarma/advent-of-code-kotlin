6d26795adfcac6c430553893cbbf90f5ca422294
Jadarma <dancristiancimpianu@gmail.com>
2023-04-01 22:51:04 +0300

    Ignored unused warning on project test configuration.

--- example/src/test/kotlin/TestConfig.kt
+++ example/src/test/kotlin/TestConfig.kt
@@ -2,6 +2,7 @@ import io.github.jadarma.aockt.test.AocktDisplayNameExtension
 import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.engine.test.names.DefaultDisplayNameFormatter
 
+@Suppress("Unused")
 object TestConfig : AbstractProjectConfig() {
     override fun extensions() = listOf(
         AocktDisplayNameExtension(DefaultDisplayNameFormatter())
