3090b92bc2586ff633ae8cfa34234f9ec2a17f34
Jadarma <dancristiancimpianu@gmail.com>
2023-04-04 01:29:42 +0300

    Allow specs to be run in parallel.

--- example/src/test/kotlin/TestConfig.kt
+++ example/src/test/kotlin/TestConfig.kt
@@ -1,10 +1,18 @@
 import io.github.jadarma.aockt.test.AocktDisplayNameExtension
+import io.kotest.common.ExperimentalKotest
 import io.kotest.core.config.AbstractProjectConfig
+import io.kotest.core.extensions.Extension
 import io.kotest.engine.test.names.DefaultDisplayNameFormatter
 
 @Suppress("Unused")
+@ExperimentalKotest
 object TestConfig : AbstractProjectConfig() {
-    override fun extensions() = listOf(
+
+    override fun extensions() = listOf<Extension>(
         AocktDisplayNameExtension(DefaultDisplayNameFormatter())
     )
+
+    private val cores = Runtime.getRuntime().availableProcessors()
+    override val parallelism: Int = cores
+    override val concurrentSpecs: Int = cores
 }
