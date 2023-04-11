65c467bb1046aa2c75d0cb7584a45a9c5a12f818
Jadarma <dancristiancimpianu@gmail.com>
2023-04-11 18:41:49 +0300

    Add test for multiple solutions.

---
+++ aockt-test/src/test/kotlin/io/github/jadarma/aockt/test/integration/MultipleSolutions.kt
@@ -0,0 +1,60 @@
+package io.github.jadarma.aockt.test.integration
+
+import io.github.jadarma.aockt.core.Solution
+import io.github.jadarma.aockt.test.AdventDay
+import io.github.jadarma.aockt.test.AdventSpec
+
+/** A solution to a fictitious puzzle used for testing using collections. */
+class Y9999D01UsingCollections : Solution {
+
+    private fun parseInput(input: String): List<Int> =
+        input
+            .splitToSequence(',')
+            .map(String::toInt)
+            .toList()
+
+    override fun partOne(input: String) = parseInput(input).filter { it % 2 == 1 }.sum()
+
+    override fun partTwo(input: String) = parseInput(input).reduce { a, b -> a * b }
+}
+
+/** A solution to a fictitious puzzle used for testing using sequences. */
+class Y9999D01UsingSequences : Solution {
+
+    private fun parseInput(input: String): Sequence<Int> =
+        input
+            .splitToSequence(',')
+            .map(String::toInt)
+
+    override fun partOne(input: String) = parseInput(input).filter { it % 2 == 1 }.sum()
+
+    override fun partTwo(input: String) = parseInput(input).reduce { a, b -> a * b }
+}
+
+@AdventDay(9999, 1, "Magic Numbers","Using Collections")
+class Y9999D01CollectionsTest : Y9999D01Spec<Y9999D01UsingCollections>()
+
+@AdventDay(9999, 1, "Magic Numbers", "Using Sequences")
+class Y9999D01SequencesTest : Y9999D01Spec<Y9999D01UsingSequences>()
+
+/**
+ * A test for a fictitious puzzle.
+ *
+ * ```text
+ * The input is a string of numbers separated by a comma.
+ * Part 1: Return the sum of the odd numbers.
+ * Part 2: Return the product of the numbers.
+ * ```
+ */
+abstract class Y9999D01Spec<T : Solution> : AdventSpec<T>({
+
+    partOne {
+        "1,2,3" shouldOutput 4
+        listOf("0", "2,4,6,8", "2,2,2,2") shouldAllOutput 0
+        "1,2,5" shouldOutput 6
+    }
+
+    partTwo {
+        "1,2,3" shouldOutput 6
+    }
+})
