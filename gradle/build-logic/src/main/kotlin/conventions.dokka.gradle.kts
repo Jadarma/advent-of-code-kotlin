d7cf1986b4be765749ca32c7de36953f0f95345e
Jadarma <dancristiancimpianu@gmail.com>
2025-09-13 20:00:34 +0300

    Update documentation generation.

--- gradle/build-logic/src/main/kotlin/conventions.dokka.gradle.kts
+++ gradle/build-logic/src/main/kotlin/conventions.dokka.gradle.kts
@@ -1,19 +1,59 @@
-import org.gradle.kotlin.dsl.withType
-import org.jetbrains.dokka.gradle.DokkaTask
+import org.gradle.internal.extensions.stdlib.capitalized
+import org.jetbrains.dokka.gradle.engine.parameters.DokkaSourceSetSpec
+import org.jetbrains.dokka.gradle.engine.parameters.KotlinPlatform
+import org.jetbrains.dokka.gradle.engine.parameters.VisibilityModifier
+import java.time.LocalDate
 
 plugins {
     id("org.jetbrains.dokka")
 }
 
-tasks.withType<DokkaTask>().configureEach {
-    dokkaSourceSets.configureEach {
-        jdkVersion.set(CompileOptions.Java.languageVersion.asInt())
-        noStdlibLink.set(true)
+dokka {
+    moduleName = "AocKt " + project.name
+        .removePrefix("aockt-")
+        .replace('-', ' ')
+        .capitalized()
+
+    dokkaSourceSets.named<DokkaSourceSetSpec>("main") {
+
+        val docVersion = buildVersion.get()
+        if (docVersion.isRelease) {
+            sourceLink {
+                val sourceTree = "v${docVersion.version}"
+                val subProject = project.name
+                remoteUrl("https://github.com/Jadarma/advent-of-code-kotlin/tree/$sourceTree/$subProject")
+                remoteLineSuffix = "#L"
+            }
+        }
+
+        analysisPlatform = KotlinPlatform.JVM
+        reportUndocumented = true
+        documentedVisibilities = setOf(VisibilityModifier.Public)
+        enableJdkDocumentationLink = false
+        enableKotlinStdLibDocumentationLink = false
+        jdkVersion = CompileOptions.Java.languageVersion.asInt()
+        languageVersion = CompileOptions.Kotlin.languageVersion.version
+        apiVersion = CompileOptions.Kotlin.apiVersion.version
     }
+
+    dokkaPublications.html {
+        offlineMode = true
+        suppressObviousFunctions = true
+        suppressInheritedMembers = true
+    }
+
+    pluginsConfiguration.html {
+        separateInheritedMembers = true
+        homepageLink = "https://jadarma.github.io/advent-of-code-kotlin"
+        footerMessage = "(c) 2020-${LocalDate.now().year} Dan Cîmpianu"
+    }
+
+    dokkaGeneratorIsolation = ClassLoaderIsolation()
 }
 
-tasks.register<Jar>("dokkaJavadocJar") {
-    dependsOn(tasks.dokkaHtml)
-    from(tasks.dokkaHtml.flatMap { it.outputDirectory })
+tasks.register<Jar>("javadocJar") {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles the Javadoc Jar."
+    from(tasks.dokkaGeneratePublicationHtml.flatMap { it.outputDirectory })
     archiveClassifier.set("javadoc")
 }
