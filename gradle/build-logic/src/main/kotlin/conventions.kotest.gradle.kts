001d90ac307217f144fa2effcb0dcc0a8964b11a
Jadarma <dancristiancimpianu@gmail.com>
2025-09-16 21:58:55 +0300

    Configure test coverage with Kover.

--- gradle/build-logic/src/main/kotlin/conventions.kotest.gradle.kts
+++ gradle/build-logic/src/main/kotlin/conventions.kotest.gradle.kts
@@ -4,12 +4,34 @@ import org.gradle.kotlin.dsl.withType
 
 plugins {
     kotlin("jvm")
+    id("org.jetbrains.kotlinx.kover")
+}
+
+dependencies {
+    testImplementation(libs.kotest.runner)
+    testImplementation(libs.kotest.property)
+    testImplementation(libs.kotest.assertions)
+}
+
+kover {
+    currentProject {
+        sources {
+            includedSourceSets = setOf("main")
+        }
+    }
+    reports {
+        total {
+            html {
+                onCheck = true
+                charset = "UTF-8"
+            }
+            binary {
+                onCheck = true
+            }
+        }
+    }
 }
 
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
-}
-
-dependencies {
-    testImplementation(libs.bundles.kotest)
 }
