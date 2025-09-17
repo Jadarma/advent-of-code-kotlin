46b77d781ce24040c7bff68699e61e2f6447bf10
Jadarma <dancristiancimpianu@gmail.com>
2025-09-18 00:12:53 +0300

    Refactor.

--- gradle/build-logic/src/main/kotlin/VersionCatalogWorkaround.kt
+++ gradle/build-logic/src/main/kotlin/VersionCatalogWorkaround.kt
@@ -1,13 +1,10 @@
 import org.gradle.accessors.dm.LibrariesForLibs
 import org.gradle.api.Project
-import org.gradle.api.plugins.ExtensionAware
+import org.gradle.kotlin.dsl.getByName
 
 /**
  * VooDoo magic workaround for accessing Gradle Version Catalogs from within precompiled convention scripts.
  * Also see: https://github.com/gradle/gradle/issues/15383
  */
 val Project.libs: LibrariesForLibs get() =
-    (rootProject.project as ExtensionAware)
-        .extensions
-        .getByName("libs")
-        .let { it as LibrariesForLibs }
+    rootProject.project.extensions.getByName<LibrariesForLibs>("libs")
