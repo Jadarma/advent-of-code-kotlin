ec37957131f756cbe161dc4307d34f0baa52f6bf
Jadarma <dancristiancimpianu@gmail.com>
2026-03-28 21:14:30 +0200

    Cleanup.

--- aockt-test/src/main/kotlin/AocKtDsl.kt
+++ aockt-test/src/main/kotlin/AocKtDsl.kt
@@ -62,6 +62,60 @@ public interface AdventRootScope {
      * If used, ignores all other tests, and only runs this [test] lambda.
      * The solution instance being tested is exposed in the scope.
      * Useful when running the test in debug mode, to ensure only this test will trigger any breakpoints.
+     *
+     * @param test A context to execute a debug run in.
      */
     public fun debug(test: AdventDebugScope.() -> Unit)
+}
+
+/** A DSL scope for defining example assertions for puzzle parts. */
+@AocKtDsl
+public interface AdventPartScope {
+
+    /**
+     * Creates a new test that asserts that when given this string as input, it gets the correct [expected] answer.
+     * The [expected] value can be anything and will be tested against its `.toString()` value.
+     *
+     * @receiver The example puzzle input.
+     * @param expected The correct answer to the puzzle for the given input.
+     */
+    public infix fun String.shouldOutput(expected: Any)
+
+    /**
+     * For each of the values given creates a new test that asserts that when given as input, it gets the correct
+     * [expected] answer.
+     * The [expected] value can be anything and will be tested against its `.toString()` value.
+     *
+     * _NOTE:_ This should be equivalent to calling [shouldOutput] for every input.
+     *
+     * @receiver The example puzzle inputs.
+     * @param expected The correct answer to the puzzle for all given inputs.
+     */
+    public infix fun Iterable<String>.shouldAllOutput(expected: Any)
+}
+
+/** A DSL scope for defining an isolated run for debugging. */
+@AocKtDsl
+public interface AdventDebugScope {
+
+    /** The instance of the solution being tested. */
+    public val solution: Solution
+
+    /**
+     * The actual puzzle input for this puzzle.
+     * Reading this property while the input is not available results in an error.
+     */
+    public val input: String
+}
+
+/** Configures which inputs the tests will run on. */
+public enum class ExecMode {
+    /** Run both tests and the user input, if available. */
+    All,
+
+    /** Do not run the user input, even if available. */
+    ExamplesOnly,
+
+    /** Do not run the defined examples. */
+    SkipExamples;
 }
--- aockt-test/src/main/kotlin/AocKtExtension.kt
+++ aockt-test/src/main/kotlin/AocKtExtension.kt
@@ -11,7 +11,6 @@ import io.kotest.core.extensions.SpecExtension
 import io.kotest.core.spec.Spec
 import io.kotest.core.spec.SpecRef
 import io.kotest.engine.names.DisplayNameFormatter
-import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.withContext
 import kotlin.time.Duration
 
@@ -27,18 +26,20 @@ import kotlin.time.Duration
  * ```
  *
  * @param efficiencyBenchmark What is the maximum runtime a solution can have while being considered efficient by
- *   the time tests.
- *   Can be overridden on a per-test basis.
- *   According to the AoC website, all solutions *should* be completable in 15 seconds regardless of hardware.
- *   You may decrease this value for an increased challenge though do be aware of fluctuations in execution time due to
- *   JVM warmup.
- *   Default is fifteen seconds.
- * @param executionMode The default execution mode for puzzle part definitions.
- *   Can be overridden on a per-test basis.
- *   If set to `ExamplesOnly`, does not run against the true puzzle input even if present.
- *   Useful when running the project with encrypted inputs (e.g. running a clone of someone else's solution repo).
- *   If set to `SkipExamples`, will only test against user input.
- *   Default is `All`.
+ *                            the time tests.
+ *                            Can be overridden on a per-test basis.
+ *                            According to the AoC website, all solutions *should* be completable in 15 seconds
+ *                            regardless of hardware.
+ *                            You may decrease this value for an increased challenge though do be aware of fluctuations
+ *                            in execution time due to JVM warmup.
+ *                            Default is fifteen seconds.
+ * @param executionMode       The default execution mode for puzzle part definitions.
+ *                            Can be overridden on a per-test basis.
+ *                            If set to `ExamplesOnly`, does not run against the true puzzle input even if present.
+ *                            Useful when running the project with encrypted inputs (e.g. running a clone of someone
+ *                            else's solution repo).
+ *                            If set to `SkipExamples`, will only test against user input.
+ *                            Default is `All`.
  */
 public class AocKtExtension(
     efficiencyBenchmark: Duration = AdventProjectConfig.Default.efficiencyBenchmark,
@@ -60,21 +61,9 @@ public class AocKtExtension(
         execute: suspend (Spec) -> Unit,
     ) {
         if (spec is AdventSpec<*>) {
-            withContext(currentCoroutineContext() + configuration) { execute(spec) }
+            withContext(configuration) { execute(spec) }
         } else {
             execute(spec)
         }
     }
-}
-
-/** Configures which inputs the tests will run on. */
-public enum class ExecMode {
-    /** Run both tests and the user input, if available. */
-    All,
-
-    /** Do not run the user input, even if available. */
-    ExamplesOnly,
-
-    /** Do not run the defined examples. */
-    SkipExamples;
 }
